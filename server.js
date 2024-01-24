09d7a9daf63795c75c4943675ad51e3dfcc9837c
Parth <parth@halalbox.in>
2024-01-24 21:53:09 +0530

    Added caching, dotenv, env .

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
+require('dotenv').config();
 var prerender = require('./lib');
+var prerenderOpts = {
+    chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--hide-scrollbars'],
+}
+if (process.env.CHROME_LOCATION) prerenderOpts.chromeLocation = process.env.CHROME_LOCATION;
 
 var server = prerender();
 
+server.use(require('prerender-memory-cache'));
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.browserForceRestart());
 // server.use(prerender.blockResources());
